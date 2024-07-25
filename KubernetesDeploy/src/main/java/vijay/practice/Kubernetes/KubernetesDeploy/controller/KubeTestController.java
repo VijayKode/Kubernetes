6482683d0e8279722817221823548865a9dd4d3d
Vijaysambasivarao Kode <vijaysambasivarao_kode@epam.com>
2024-07-25 13:59:46 +0530

    added some random code to increase some processing time

--- KubernetesDeploy/src/main/java/vijay/practice/Kubernetes/KubernetesDeploy/controller/KubeTestController.java
+++ KubernetesDeploy/src/main/java/vijay/practice/Kubernetes/KubernetesDeploy/controller/KubeTestController.java
@@ -1,14 +1,20 @@
 package vijay.practice.Kubernetes.KubernetesDeploy.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class KubeTestController {
 
     @GetMapping("/hello")
     ResponseEntity<String> greet() {
+        log.info("Print Number from 1 to 5000");
+        for(int i =0; i < 50000; i++) {
+           log.info("{}",i);
+        }
         return ResponseEntity.ok("Hello Guys");
     }
 
